f7e3802f10e5f223480867bbac6a59362cbd28d0
hrefDeveloperGH <paiafellinluv999@gmail.com>
2023-06-15 19:28:11 -0300

    pfv deixa as coisas fluirem

---
+++ segunda tela/dinamic.js
@@ -0,0 +1,84 @@
+const opium = {
+    id: '0',
+    namemusic : 'if looks could kill',
+    artista : 'Destroy Lonely',
+    album: 'single',
+    coverFile : 'opium_friday.jpg',
+};
+
+const ugliest = {
+    id: '1',
+    namemusic : 'Ugliest',
+    artista : '$UICIDEBOY$',
+    album : 'Long Term Effects of Suffering',
+    coverFile : 'ugliest.jpg',
+};
+
+const ghostKilla = {
+    id: '2',
+    namemusic : 'GHOSTKILLA',
+    artista : '1nonly', 
+    album : 'single',
+    coverFile : 'ghost_Killa.jpg',
+};
+
+const corsaFreestyle = {
+    id: '3',
+    namemusic : 'Corsa Freestyle',
+    artista : 'Ryu, the Runner',
+    album : 'Essa é a Vida de um Corredor',
+    coverFile : 'corsa_Freestyle.jpg',
+};
+
+const sempreTravado = {
+    id: '4',
+    namemusic : 'SEMPRE TRAVADO',
+    artista : 'DEREK',
+    album: 'single',
+    coverFile : 'sempre_travado.jpg',
+};
+
+const musicBiblioteca = [
+    opium, 
+    ugliest,
+    ghostKilla, 
+    corsaFreestyle, 
+    sempreTravado,
+];
+
+let songsPlaylist = [...musicBiblioteca]
+
+const pageBody = document.getElementById('corpinho');
+const pesquisar = document.getElementById('search-dcria');
+const buttonSearch = document.getElementById('search-button');
+
+
+function loadBiblioteca(){
+    pageBody.innerHTML = '';
+    for(let index = 0; index < songsPlaylist.length; index++)  {
+        pageBody.innerHTML += `
+        <div class="card d-flex flex-column align-items-center" 
+        style="width: 18rem; height: 30rem;"
+        >
+            <img src="covers/${musicBiblioteca[index].coverFile}"
+             class="card-img-top" 
+             alt="Disc Cover"
+             />
+            <div class="card-body d-flex flex-column">
+              <h5 class="card-title">${musicBiblioteca[index].namemusic}</h5>
+              <p class="card-text">${musicBiblioteca[index].artista}</p>
+              <p class="card-text">${musicBiblioteca[index].album}</p>
+              <button class="btn btn-outline-success"><i class="bi bi-play-circle"></i></button>
+            </div>
+          </div>
+        `} 
+}
+
+function searchClick(){
+    console.log(pesquisar.value);
+}
+
+buttonSearch.addEventListener('click', searchClick);
+
+
+loadBiblioteca();
